6ebf430afb0b812ac6427b955fa0c2e6d96725a6
Luc Steinmetz <s8lustei@stud.uni-saarland.de>
2019-11-27 03:51:12 +0100

    added meta-tags

--- wormermusek/src/App.vue
+++ wormermusek/src/App.vue
@@ -20,7 +20,12 @@ export default {
   // if no subcomponents specify a metaInfo.title, this title will be used
   title: 'Wormer Musek a.s.b.l.',
   // all titles will be injected into this template
-  titleTemplate: '%s | Wormer Musek a.s.b.l.'
+  titleTemplate: '%s | Wormer Musek a.s.b.l.',
+  meta: [
+      { name: "description", content: "Häerzlech Wëllkomm bei der Wormer Musek! Eng cool, jonk an dynamesch Musek aus der Rieslingsgemeen. Mir prouwen all Freideg vun 19.30 bis 21.30 zu Wormer an eisem Prouwesall am bloen Haus vis-à-vis vun der Gemeen." },
+      { name: "keywords", content: "wormeldange, wormer, musek, musel, mosel, musik, orchester, harmonie de wormeldange, harmonie" }
+      //you can also add open graph tags here
+    ]
   },
   components : {
     Navigation, Footer1
